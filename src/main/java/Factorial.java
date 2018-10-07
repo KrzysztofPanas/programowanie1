83f5b564888425457d2a7d65e5c2f5a41a842a8e
Krzysztof Panas <panas_k@2o.pl>
2018-10-07 11:42:21 +0200

    message added

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Factorial {
+
+    public static void main(String[] args) {
+        System.out.println("Podaj liczbę do obliczenia silni:");
+        int input = new Scanner(System.in).nextInt();
+        int output = calculateFactorial(input);
+        System.out.println(input + " silnia to: " + output);
+    }
+
+    private static int calculateFactorial(int input) {
+        int wynik;
+        if (input > 1) {
+            wynik = input * calculateFactorial(input-1);
+        } else wynik = 1;
+        return wynik;
+    }
 }
